a6ca74574b3bb0dce2480c6c718a92efc51ab913
tingan <tingan@ch-si.com.tw>
2024-11-11 17:09:27 +0800

    login邏輯替換

--- src/router/controller/user.ts
+++ src/router/controller/user.ts
@@ -1,14 +1,58 @@
 import { Request, Response } from 'express'
 import { createKysely } from '@vercel/postgres-kysely'
-
+import dataPool from '../../../data_pool/index'
+import jwt from 'jsonwebtoken'
 interface Users {
     users: {
         id?: number
         account: string    // 登入帳號
-        password: string   // 密碼
+        password: string   // 密碼 (實際專案密碼必須雜湊)
         username: string   // 使用者名稱
         menu: number[]     // 使用 JSONB 存選單權限
         created_at?: Date
+    }
+}
+
+export const login = async (req: Request, res: Response) => {
+    const { account: acc, password: psw } = req.body
+    try {
+        const db = createKysely<Users>()
+        const users = await db
+            .selectFrom('users')
+            .select(['id', 'account', 'username', 'menu'])
+            .where('account', '=', acc)
+            .where('password', '=', psw)
+            .executeTakeFirst() // 只返回第一筆資料
+        if (!users) {
+            res.status(401).json({
+                success: false,
+                message: '查無此帳號'
+            })
+            return
+        } else {
+            const { routerList } = dataPool //寫死的路由資料
+            const menu: any = []
+            // 產生JWT Token
+            const token = jwt.sign(
+                users,
+                process.env.JWT_SECRET as string,
+                { expiresIn: parseInt(process.env.JWT_EXPIRE!) }
+            )
+            users.menu.forEach(el => menu.push(routerList[el - 1]))
+            res.status(200).json({
+                token,
+                success: true,
+                isPremission: true,
+                message: '登入成功',
+                usersname: users.username,
+                menu
+            })
+        }
+    } catch (error) {
+        res.status(401).json({
+            success: false,
+            message: '登入失敗'
+        })
     }
 }
 
@@ -25,7 +69,7 @@ export const addUser = async (req: Request, res: Response) => {
                 menu
             })
             .returning(['id', 'account', 'username', 'menu'])
-            .executeTakeFirst() // 只返回第一筆資料
+            .executeTakeFirst() 
         res.status(200).json({
             success: true,
             message: '新增成功',
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,30 @@
-import { Router, Request, Response } from 'express'
-import { login } from './login'
-import { pusher } from '../webSocket/index'
+import { Router } from 'express'
 import connectionDatabase from '../../data_pool/database'
-import { addUser, modifyUser } from './controller/user'
+import { addUser, modifyUser, login as loginUser } from './controller/user'
+// import { pusher } from '../webSocket/index'
+// import { login } from './login'
 
 const router = Router()
 
-router.post('/login', login)
-router.post('/ws/test', (req: Request, res: Response) => {
-    console.log('??')
-    try {
-        const socketId = req.body.socket_id;
-        const channel = req.body.channel_name;
-        console.log(socketId)
-        const auth = pusher.authenticateUser(socketId, channel)
-        console.log('auth', auth)
-        res.send(auth)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
+router.post('/login', loginUser)
 router.post('/addUser', addUser)
 router.put('/modifyUser', modifyUser)
+
+
 router.delete('/table/drop/users', connectionDatabase.dropUserTable)
 router.post('/table/create/users', connectionDatabase.createUserTable)
 
+// router.post('/ws/test', (req: Request, res: Response) => {
+//     console.log('??')
+//     try {
+//         const socketId = req.body.socket_id;
+//         const channel = req.body.channel_name;
+//         console.log(socketId)
+//         const auth = pusher.authenticateUser(socketId, channel)
+//         console.log('auth', auth)
+//         res.send(auth)
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })
 export default router;
