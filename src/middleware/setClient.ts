88bb279d039b9efa42e1856db98276b47a1f0f58
tingan <tingan@ch-si.com.tw>
2024-11-12 10:19:56 +0800

    修正檔名

--- src/middleware/setClient.ts
+++ src/middleware/setClient.ts
@@ -2,13 +2,13 @@ import express, { Express, Request, Response } from "express"
 import path from "path";
 
 export const setClient = (app: Express) => {
-    
+
     // 設置靜態檔案
     app.use(express.static('public'))
 
     // 設置靜態首頁
     app.get('/', (req: Request, res: Response) => {
-        res.sendFile(path.resolve(__dirname, '../../public', 'indexs.html'));
+        res.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
     });
 
     // 處理瀏覽器自動發出的 favicon.ico 請求
