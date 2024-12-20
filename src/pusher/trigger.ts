744f419af97859a642bc86bfe6e11dda628e1188
tingan <tingan@ch-si.com.tw>
2024-12-20 17:28:01 +0800

    trigger調整返回資料格式

--- src/pusher/trigger.ts
+++ src/pusher/trigger.ts
@@ -3,16 +3,17 @@ import pusher from "."
 // 事件通知或最新消息
 export const triggerNotification = (type: 'news' | 'warning') => {
     try {
-        const date = new Date().toLocaleString()
+        const date = new Date()
         const eventContent = {
             type: 'info',
             title: '最新通知',
-            message: '通知測試,每5秒送一次 ' + date
+            message: '通知測試,每5秒送一次',
+            time: date
         }
         if (type === 'warning') {
             eventContent.type = 'warning'
             eventContent.title = '警告'
-            eventContent.message = "警告測試,每10秒送一次 " + date
+            eventContent.message = "警告測試,每10秒送一次"
         }
         pusher.trigger('notification', 'update', eventContent)
     } catch (error) {
