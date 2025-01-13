f4424fcc46145529605d263b4d502402a5a5bd94
tingan <tingan@ch-si.com.tw>
2025-01-13 14:03:36 +0800

    處理路徑定義問題

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,10 @@
 import * as path from 'path'
 import * as webpack from 'webpack'
 import nodeExternals from 'webpack-node-externals'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const config: webpack.Configuration = {
     mode: 'production',
